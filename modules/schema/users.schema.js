ce315688d43d5ee122a73f39bee9fe4dabaabfe5
PersonalAccessToken <omarksm09@gmail.com>
2021-11-04 01:18:48 +0200

    schema update

--- modules/schema/users.schema.js
+++ modules/schema/users.schema.js
@@ -1,8 +1,9 @@
 const mongoose =require('mongoose');
 const userSchema = mongoose.Schema({
-    userName : { String, required},
-    email : { String, required},
-    password : { String, required}
+    userName : { type:String , required: true},
+    email : {type:String , required: true},
+    password : {type:String , required: true},
+    date:{type:Date,Default: Date.now}
 
 })
 
